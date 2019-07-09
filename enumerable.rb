f3b781a3326edbbdc30afcff0019e15daa0b81ef
lilleonz <lilleonz@gmail.com>
2019-07-09 17:58:14 -0400

    trying to fix linting error of missing end which it has

--- enumerable.rb
+++ enumerable.rb
@@ -1,164 +1,73 @@
-# enumerable
-# frozen_string_literal: true
-
 module Enumerable
   def my_each
-    i = 0
-      while i < self.size
-        yield(self[i], 1)
-          i += 1
-      end
+    for i in 0...self.length
+      yield(self[i])
   end
+
   def my_each_with_index
-    i = 0
-      while i < self.size
-        yield(self[i], i)
-          i += 1
-      end
+    for i in 0...self.length
+      yield(self[i], i)
   end
+
   def my_select
-    result = []
-      self.my_each do |e|
-          if yield e
-              result << e
-          end
-      end
-      result
+    newCollection = []
+    self.my_each { |item| newCollection.push(item) if yield(item) } 
+    return newCollection
   end
+
   def my_all?
-    result = true
-      self.my_each do |e|
-        result = false unless yield e
-      end
-      result
+    output = true
+    self.my_each { |item| output = false unless yield(item) }
   end
+
   def my_any?
-    result = false
-      self.my_each do |e|
-        if yield e
-          result = true
-            break
-          end
-      end
-      result
+    output = false
+    self.my_each { |item| output = true if yield(item) }
+    return output
   end
+
   def my_none?
-    result = true
-      self.my_each do |e|
-        if yield e
-          result = false
-            break
-        end
-      end
-      result
+    output = true
+    self.my_each { |item| output = false if yield(item) }
+    return output
   end
-  def my_count num = nil
+
+  def my_count(num = nil)
     count = 0
-      if num
-        self.my_each do |e|
-          count += 1 if e == num
-        end
-      else
-        self.my_each do |e|
-          count += 1 if yield e
-        end
-      end
-      count
+    if item != nil
+      self.my_each { |element| count += 1 if element == item }
+    elsif block_given?
+      self.my_each { |element| count += 1 if yield(element) }
+    else
+      count = self.length
+    end
+    return count    
   end
-  def my_map proc = nil
-    result = []
-      if proc
-        self.my_each_with_index do |e, i|
-          result[i] = proc.call(e)
-        end
-      else
-        self.my_each_with_index do |e, i|
-          result[i] = yield e
-        end
-      end
-      result
+
+  def my_map(proc = nil)
+    if block == nil
+      self.my_each_with_index { |element, index| self[index] = yield(element) }
+    elsif block != nil && block_given?
+      self.my_each_with_index { |element, index| self[index] = block.call(element) }
+    else block
+      self.my_each_with_index { |element, index| self[index] = block.call(element) }    
+    end
+  return self
   end
-  def my_inject *args
-    case args.length
-      when 1
-        if args[0].class == Symbol
-          memo = self[0]
-            self.my_each_with_index do |e, i|
-              next if i == 0
-                memo = memo.method(args[0]).call(e)
-              end
-        else
-          memo = args[0]
-            self.my_each do |e|
-              memo = yield(memo, e)
-            end
-        end
-      when 2
-        memo = args[0]
-          self.my_each do |e|
-            memo = memo.method(args[1]).call(e)
-          end
-      else
-        memo = self[0]
-          self.my_each_with_index do |e, i|
-            next if i  == 0
-              memo = yield(memo, e)
-          end
-      end
-      memo
+
+  def my_inject(intial = nil)
+    if intial == nil
+      accumulator = self.first
+    else
+      accumulator = intial
+    end
+    self.my_each do |element|
+      accumulator =yield(accumulator, element)
+    end
+    return accumulator
   end
 end
 
-def multiply_els array
-    array.my_inject(:*)
+def multiply_els(array)
+  return array.my_inject(1) { |acc, e| acc * e }
 end
-
-
-my_array = (1..4).to_a
-
-# EACH
-my_array.each{ |e| p e }
-my_array.my_each{ |e| p e }
-
-# EACH_WITH_INDEX
-my_array.each_with_index{ |e, i| p "element: #{e}, index: #{i}" }
-my_array.my_each_with_index{ |e, i| p "element: #{e}, index: #{i}" }
-
-# SELECT
-p my_array.select{ |item| item%2==0 }
-p my_array.my_select{ |item| item%2==0 }
-
-# ALL?
-p my_array.all?{ |e| e > 1 }
-p my_array.my_all?{ |e| e > 1 }
-
-# ANY?
-p my_array.any?{ |e| e < 1 }
-p my_array.my_any?{ |e| e < 1 }
-
-# NONE?
-p my_array.none?{ |e| e > 1 }
-p my_array.my_none?{ |e| e > 1 } 
-
-# COUNT
-p my_array.count { |e| e%2 == 1 }
-p my_array.my_count { |e| e%2 == 1 }
-p my_array.count 3
-p my_array.my_count 3
-
-# MAP
-p my_array.map { |e| e ** 2 }
-p my_array.my_map { |e| e ** 2 }
-proc = Proc.new { |e| e ** 2 }
-p my_array.my_map(proc)
-
-# INJECT
-p my_array.inject(:+)
-p my_array.my_inject(:+)
-p my_array.inject(1) { |product, n| product * n }
-p my_array.my_inject(1) { |product, n| product * n }
-p my_array.inject(1, :*)
-p my_array.my_inject(1, :*)          
-p my_array.inject{ |sum, n| sum + n }
-p my_array.my_inject{ |sum, n| sum + n }
-p multiply_els([2,4,5])
