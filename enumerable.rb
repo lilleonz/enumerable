6809ffb98ec53fd2f79b891fb5c3beaa063b9f16
lilleonz <lilleonz@gmail.com>
2019-07-09 18:35:11 -0400

    finally fixed stickler issue

--- enumerable.rb
+++ enumerable.rb
@@ -2,37 +2,40 @@ module Enumerable
   def my_each
     for i in 0...self.length
       yield(self[i])
+    end
   end
 
   def my_each_with_index
     for i in 0...self.length
       yield(self[i], i)
+    end
   end
 
   def my_select
     newCollection = []
     self.my_each { |item| newCollection.push(item) if yield(item) } 
-    return newCollection
+    newCollection
   end
 
   def my_all?
     output = true
     self.my_each { |item| output = false unless yield(item) }
+    output
   end
 
   def my_any?
     output = false
     self.my_each { |item| output = true if yield(item) }
-    return output
+    output
   end
 
   def my_none?
     output = true
     self.my_each { |item| output = false if yield(item) }
-    return output
+    output
   end
 
-  def my_count(num = nil)
+  def my_count(item = nil)
     count = 0
     if item != nil
       self.my_each { |element| count += 1 if element == item }
@@ -41,18 +44,18 @@ module Enumerable
     else
       count = self.length
     end
-    return count    
+    count
   end
 
-  def my_map(proc = nil)
+  def my_map(block = nil)
     if block == nil
       self.my_each_with_index { |element, index| self[index] = yield(element) }
     elsif block != nil && block_given?
       self.my_each_with_index { |element, index| self[index] = block.call(element) }
-    else block
+    elsif block
       self.my_each_with_index { |element, index| self[index] = block.call(element) }    
     end
-  return self
+  self
   end
 
   def my_inject(intial = nil)
@@ -64,10 +67,18 @@ module Enumerable
     self.my_each do |element|
       accumulator =yield(accumulator, element)
     end
-    return accumulator
+    accumulator
   end
 end
 
 def multiply_els(array)
-  return array.my_inject(1) { |acc, e| acc * e }
+  array.my_inject(1) { |acc, e| acc * e }
 end
+
+var1 = multiply_els([2,4,5])
+print var1
+puts ""
+
+block = Proc.new{|string| string.upcase}
+var2 = ["a", "b", "c"].my_map(block)
+print var2
