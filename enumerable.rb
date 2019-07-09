a0bc7aa6147a7d1edcb8654a234bf37f7a762ca5
lilleonz <lilleonz@gmail.com>
2019-07-08 20:46:20 -0400

    adjusted linting issues

--- enumerable.rb
+++ enumerable.rb
@@ -1,117 +1,109 @@
 module Enumerable
-    def my_each
-        i = 0
-        while i < self.size
-            yield(self[i], 1)
-            i += 1
+  def my_each
+    i = 0
+      while i < self.size
+        yield(self[i], 1)
+          i += 1
+      end
+  end
+  def my_each_with_index
+    i = 0
+      while i < self.size
+        yield(self[i], i)
+          i += 1
+      end
+  end
+  def my_select
+    result = []
+      self.my_each do |e|
+          if yield e
+              result << e
+          end
+      end
+      result
+  end
+  def my_all?
+    result = true
+      self.my_each do |e|
+        result = false unless yield e
+      end
+      result
+  end
+  def my_any?
+    result = false
+      self.my_each do |e|
+        if yield e
+          result = true
+            break
+          end
+      end
+      result
+  end
+  def my_none?
+    result = true
+      self.my_each do |e|
+        if yield e
+          result = false
+            break
         end
-    end
-
-    def my_each_with_index
-        i = 0
-        while i < self.size
-            yield(self[i], i)
-            i += 1
-        end
-    end
-
-    def my_select
-        result = []
+      end
+      result
+  end
+  def my_count num = nil
+    count = 0
+      if num
         self.my_each do |e|
-            if yield e
-                result << e
-            end
+          count += 1 if e == num
         end
-        result
-    end
-
-    def my_all?
-        result = true
+      else
         self.my_each do |e|
-            result = false unless yield e
+          count += 1 if yield e
         end
-        result
-    end
-
-    def my_any?
-        result = false
-        self.my_each do |e|
-            if yield e
-                result = true
-                break
-            end
+      end
+      count
+  end
+  def my_map proc = nil
+    result = []
+      if proc
+        self.my_each_with_index do |e, i|
+          result[i] = proc.call(e)
         end
-        result
-    end
-
-    def my_none?
-        result = true
-        self.my_each do |e|
-            if yield e
-                result = false
-                break
-            end
+      else
+        self.my_each_with_index do |e, i|
+          result[i] = yield e
         end
-        result
-    end
-
-    def my_count num = nil
-        count = 0
-        if num
-            self.my_each do |e|
-                count += 1 if e == num
-            end
-        else
-            self.my_each do |e|
-                count += 1 if yield e
-            end
-        end
-        count
-    end
-
-    def my_map proc = nil
-        result = []
-        if proc
+      end
+      result
+  end
+  def my_inject *args
+    case args.length
+      when 1
+        if args[0].class == Symbol
+          memo = self[0]
             self.my_each_with_index do |e, i|
-                result[i] = proc.call(e)
-            end
+              next if i == 0
+                memo = memo.method(args[0]).call(e)
+              end
         else
-            self.my_each_with_index do |e, i|
-                result[i] = yield e
-            end
-        end
-        result
-    end
-
-    def my_inject *args
-        case args.length
-        when 1
-            if args[0].class == Symbol
-                memo = self[0]
-                self.my_each_with_index do |e, i|
-                    next if i == 0
-                    memo = memo.method(args[0]).call(e)
-                end
-            else
-                memo = args[0]
-                self.my_each do |e|
-                    memo = yield(memo, e)
-                end
-            end
-        when 2
-            memo = args[0]
+          memo = args[0]
             self.my_each do |e|
-                memo = memo.method(args[1]).call(e)
-            end
-        else
-            memo = self[0]
-            self.my_each_with_index do |e, i|
-                next if i  == 0
-                memo = yield(memo, e)
+              memo = yield(memo, e)
             end
         end
-        memo
-    end
+      when 2
+        memo = args[0]
+          self.my_each do |e|
+            memo = memo.method(args[1]).call(e)
+          end
+      else
+        memo = self[0]
+          self.my_each_with_index do |e, i|
+            next if i  == 0
+              memo = yield(memo, e)
+          end
+      end
+      memo
+  end
 end
 
 def multiply_els array
