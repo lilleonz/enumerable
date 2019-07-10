2e78c9eb076293aaeb882e9c107c0a960d51e945
lilleonz <lilleonz@gmail.com>
2019-07-09 21:37:06 -0400

    think i got the linter issues fixed

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,7 @@
+# This is a project for Microverse
+# By Leon Little
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     for i in 0...self.length
@@ -7,78 +11,101 @@ module Enumerable
 
   def my_each_with_index
     for i in 0...self.length
-      yield(self[i], i)
+      yield(self[i],i)
     end
   end
 
   def my_select
-    newCollection = []
-    self.my_each { |item| newCollection.push(item) if yield(item) } 
-    newCollection
+    new_collection = []
+    my_each { |item| new_collection << item if yield(item) }
+    new_collection
   end
 
   def my_all?
     output = true
-    self.my_each { |item| output = false unless yield(item) }
+    my_each { |item| output = false unless yield item }
     output
   end
 
   def my_any?
     output = false
-    self.my_each { |item| output = true if yield(item) }
+    my_each { |item| output = true if yield(item) }
     output
   end
 
   def my_none?
     output = true
-    self.my_each { |item| output = false if yield(item) }
+    my_each { |item| output = false if yield(item) }
     output
   end
 
   def my_count(item = nil)
     count = 0
-    if item != nil
-      self.my_each { |element| count += 1 if element == item }
+    if !item.nil?
+      my_each { |element| count += 1 if element == item }
     elsif block_given?
-      self.my_each { |element| count += 1 if yield(element) }
+      my_each { |element| count += 1 if yield(element) }
     else
-      count = self.length
+      count = length
     end
     count
   end
 
   def my_map(block = nil)
-    if block == nil
-      self.my_each_with_index { |element, index| self[index] = yield(element) }
-    elsif block != nil && block_given?
-      self.my_each_with_index { |element, index| self[index] = block.call(element) }
+    if block == nil?
+      my_each_with_index{ |element,index| self[index] = yield(element) }
+    elsif !block.nil? && block_given?
+      my_each_with_index{ |element,index| self[index] = block.call(element) }
     elsif block
-      self.my_each_with_index { |element, index| self[index] = block.call(element) }    
+      my_each_with_index{ |element,index| self[index] = block.call(element) }
     end
-  self
+    self
   end
 
-  def my_inject(intial = nil)
-    if intial == nil
-      accumulator = self.first
-    else
-      accumulator = intial
-    end
-    self.my_each do |element|
-      accumulator =yield(accumulator, element)
+  def my_inject(initial = nil)
+    accumulator =
+      if initial.nil?
+        first
+      else
+        initial
+      end
+    my_each do |element|
+      yield(accumulator, element)
     end
     accumulator
   end
 end
 
-def multiply_els(array)
-  array.my_inject(1) { |acc, e| acc * e }
+def multiply_els(number_list)
+  number_list.my_inject(1) { |x, y| x * y }
 end
 
-var1 = multiply_els([2,4,5])
-print var1
-puts ""
+# Test cases
+test_array = Array.new(10) { rand(0..100) }
 
-block = Proc.new{|string| string.upcase}
-var2 = ["a", "b", "c"].my_map(block)
-print var2
+# Each
+test_array.my_each { |value| puts "#{value} " }
+
+# Each with Index
+test_array.my_each_with_index { |value, index| puts "Index: #{index} - Value: #{value}" }
+
+# Select
+puts test_array.my_select { |value| (value % 2) == 0 }
+
+# All
+puts test_array.my_all? { |value| value > 30 }
+
+# Any
+puts test_array.my_any? { |value| value == 0 }
+
+# None
+puts test_array.my_none? { |value| value < 10 }
+
+# Count
+puts test_array.my_count { |value| (value % 2) != 0 }
+
+# Map
+puts test_array.my_map { |value| value > 50 }
+
+# Inject
+puts multiply_els([2,4,5])
